07bfdc17b0f88796ad7dd2e2a6b102de05d10d21
Fausti12 <fpettinari@udesa.edu.ar>
2025-03-11 17:01:54 -0300

    freeCellsT hecho

--- tp1/Truck.hs
+++ tp1/Truck.hs
@@ -1,4 +1,4 @@
-module Truck ( Truck, newT)--, freeCellsT)--, loadT, unloadT, netT )
+module Truck ( Truck, newT, freeCellsT)--, loadT, unloadT, netT )
   where
 
 import Palet
@@ -16,6 +16,12 @@ createStackList numBays height  = [newS height] ++ createStackList (numBays - 1)
 newT numBays height route = Tru (createStackList numBays height) route
 
 freeCellsT :: Truck -> Int            -- responde la celdas disponibles en el camion
+sumCells :: [Stack] -> Int -> Int
+sumCells stackL 0 = freeCellsS(stackL !! 0)
+sumCells stackL n = freeCellsS(stackL !! n) + sumCells stackL (n-1)
+freeCellsT (Tru stackL _) = sumCells stackL (length(stackL)-1)
+
+
 --loadT :: Truck -> Palet -> Truck      -- carga un palet en el camion
 --unloadT :: Truck -> String -> Truck   -- responde un camion al que se le han descargado los paletes que podían descargarse en la ciudad
 --netT :: Truck -> Int                  -- responde el peso neto en toneladas de los paletes en el camion
