ba8f919fb6ef8c30ef104fef7ac9a0ea38c78db1
Fausti12 <fpettinari@udesa.edu.ar>
2025-03-18 15:26:02 -0300

    corregí alguna cosa que había copiado mal

--- tp1_v2/Main.hs
+++ tp1_v2/Main.hs
@@ -20,37 +20,37 @@ testF action = unsafePerformIO $ do
 runTests :: [Bool]
 runTests = 
     [ 
-      -- Test: Crear instancia de ruta con lista vacía
+      -- Test 1: Crear instancia de ruta con lista vacía
       testF (newR [])
 
-      -- Test: Crear instancia de ruta con lista de ciudades
+      -- Test 2: Crear instancia de ruta con lista de ciudades
     , not $ testF (newR ["CiudadA", "CiudadB", "CiudadC"])
 
-      -- Test: Ciudad 1 está antes que Ciudad 2 en la ruta
+      -- Test 3: Ciudad 1 está antes que Ciudad 2 en la ruta
     , not $ testF (inOrderR route "CiudadA" "CiudadC")
 
-      -- Test: Ciudad 1 no está antes que Ciudad 2 en la ruta  VER SI ESTO IRÍA
+      -- Test 4: Ciudad 1 no está antes que Ciudad 2 en la ruta  VER SI ESTO IRÍA
     , not $ testF (inOrderR route "CiudadC" "CiudadA")
       
-      -- Test 1: Se permite subir un palet a un stack disponible
+      -- Test 5: Se permite subir un palet a un stack disponible
     ,  not $ testF (loadT truck1 paletB)
 
-      -- Test 2: No se puede subir palet porque excede peso de la bahía
+      -- Test 6: No se puede subir palet porque excede peso de la bahía
     , testF (loadT truck1 paletPesado)
 
-      -- Test 3: No se puede subir palet porque todos los stacks están llenos
+      -- Test 7: No se puede subir palet porque todos los stacks están llenos
     , testF (loadT truckFull3 paletB)
 
-      -- Test 4: No se puede subir palet porque no hay stack que lo acepte (por orden de ciudades)
+      -- Test 8: No se puede subir palet porque no hay stack que lo acepte (por orden de ciudades)
     , testF (loadT truckNotHoldsC paletC)
 
-        -- Test 5: No se puede cargar un palet que tiene destino fuera de la ruta
+        -- Test 9: No se puede cargar un palet que tiene destino fuera de la ruta
     , testF (loadT truck2 paletOutRoute)
 
-      -- Test 6: Saltearse la descarga en una ciudad, y descargar los de otra que los tiene tapados (descargar en B antes que en A)
+      -- Test 10: Saltearse la descarga en una ciudad, y descargar los de otra que los tiene tapados (descargar en B antes que en A)
     , unloadT truckTapado "CiudadB" == truckTapado  -- si está tapado, no se puede descargar; por lo tanto el camión sigue igual
 
-      -- Test 7: Descargo igual que antes ("salteándome una ciudad"), pero sin estar tapado el palet de la ciudadB
+      -- Test 11: Descargo igual que antes ("salteándome una ciudad"), pero sin estar tapado el palet de la ciudadB
     , unloadT truckFull3 "CiudadB" /= truckFull3  -- si no está tapado, se puede descargar; por lo tanto el camión queda con un palet menos
 
 
@@ -58,26 +58,28 @@ runTests =
     ]
 
 -- Definiciones para testear
-palet1 = newP "CiudadB" 3
-palet2 = newP "CiudadA" 4
+paletB = newP "CiudadB" 3
+paletA = newP "CiudadA" 4
+paletC = newP "CiudadC" 2
 paletPesado = newP "CiudadC" 50  -- Excede el peso permitido del truck
-paletOutRoute = newP "CiudadD" 3  -- Ciudad destino fuera de la ruta
+paletOutRoute = newP "CiudadE" 3  -- Ciudad destino fuera de la ruta
 
-route = newR ["CiudadA", "CiudadB", "CiudadC"]
+route = newR ["CiudadA", "CiudadB", "CiudadC", "CiudadD"]
 
 truck1 = newT 2 10 route  -- Truck con 2 Stacks de altura 10 cada uno
-truckFull = newT 2 1 route  -- Truck con 2 Stacks de altura 2 cada uno
+truckFull = newT 2 1 route  
 truck2 = newT 1 5 route  
+truck3 = newT 2 2 route
 
-truckFull2 = loadT truckFull palet1
-truckFull3 = loadT truckFull2 palet2
+truckFull2 = loadT truckFull paletB
+truckFull3 = loadT truckFull2 paletA
 
 truck4 = loadT truck3 paletA
 truckNotHoldsC = loadT truck4 paletB
 
 truckTap0 = newT 1 2 route -- este truck va a quedar con palets tapados por otros cuando los querramos descargar
-truckTap1 = loadT truckTap0 palet1
-truckTapado = loadT truckTap1 palet2
+truckTap1 = loadT truckTap0 paletB
+truckTapado = loadT truckTap1 paletA
 
 main :: IO ()
 main = print runTests
