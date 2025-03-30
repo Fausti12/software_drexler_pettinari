acf148aba6a324e7f1836ec630e8a73880856887
BryanDrexler <bdrexler@udesa.edu.ar>
2025-03-30 01:39:20 -0300

    .

---
+++ tp2/src/anillo/Ring.java
@@ -0,0 +1,55 @@
+package anillo;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Ring {
+    private Object cargo;
+    private Ring nextRing;
+
+    public Ring() {
+        nextRing = null;
+        cargo = null;
+    }
+
+    public Ring next() {
+        if ( nextRing == null ) { throw new RuntimeException( "Empty ring" );}
+        return nextRing; // Siempre retorna el siguiente nodo, aunque sea el mismo
+    }
+
+    public Object current() {
+        if ( cargo == null ) { throw new RuntimeException( "No value in ring" );}
+        return cargo;}
+
+    public Ring add( Object cargo ) {
+        Ring newRing = new Ring();
+        newRing.cargo = cargo;
+
+        if (this.nextRing == null) { // Si el anillo está vacío
+            newRing.nextRing = newRing; // Se apunta a sí mismo
+        }
+        else {
+            newRing.nextRing = this.nextRing; // Inserta en la lista
+        }
+
+        this.nextRing = newRing; //ver pq es así
+        return newRing;
+    }
+
+    public Ring remove() {
+        if ( nextRing == null ) {
+            throw new RuntimeException( "Empty ring" );
+        }
+        if (nextRing == this) { // Solo hay un nodo en el anillo
+            this.cargo = null;
+            this.nextRing = this;
+            return this;
+        }
+
+        Ring newRing = nextRing;
+        nextRing = nextRing.nextRing;
+        return newRing;
+    }
+}
